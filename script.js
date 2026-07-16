36e5e5092c7d6cb3f6026af6a1ea60e263dc1767
Jubiraci Silva S. Filho <jubiracisilva1@gmail.com>
2026-07-16 13:55:02 -0400

    feat/js: Terminar de configurar a API

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const apiKey = "";
+if (!apiKey){
+	console.error("Erro:GROQ_API_KEY não foi encontrada!")
+}
+
+let endereco = "https://api.groq.com/openai/v1/chat/completions"
+
+async function gerarCodigo(){
+	let promptTexto = document.querySelector("#prompt-usuario").value
+
+	let A = await fetch(endereco, {
+		method: "POST",
+		headers: {
+			"Content-Type" : "application/json",
+			"Authorization" :'Bearer ${apiKey}'
+	},
+		body: JSON.stringify({
+			"model": "openai/gpt-oss-120b",
+			"messages": [
+				{
+					"role": "user",
+					"content" : "promptTexto"
+				},
+				{
+					"role" : "system",
+					"content" : ""
+				}
+			],
+		})
+	})
+}
 
